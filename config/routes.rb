9d7e905912efea2a176d54ddd39e652d88c1ebfb
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-09-15 12:09:05 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
-  resources :posts do
-    resources :comments
-  end
+
+  resources :cart_items
+  resources :cart
+  resources :items
 end
