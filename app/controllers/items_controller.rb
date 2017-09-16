9633c46f443334ae4bf59848c3683333182e81fe
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-09-16 16:38:50 -0400

    got items to add to cart-items and did notifications

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,13 +12,12 @@ class ItemsController < ApplicationController
   #adding item to cart
   def add_to_cart
     @item = Item.find(params[:id])
-    @cart = Cart.new
-    @cart.cart_items.create!(item: @item)
+    current_user.cart.cart_items.create(item: @item)
     redirect_to items_path, notice: "#{@item.name} was added to the cart"
   end
 
   private
   def comment_params
-    params.require(:item).permit(:name, :pic_url)
+    params.require(:item).permit(:name, :pic_url, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'items#index'
   resources :cart_items
   resources :cart
