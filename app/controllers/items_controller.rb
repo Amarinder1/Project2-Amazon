ab69722617c301594c3d4a41bd3a303a28d59d65
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-09-18 14:43:15 -0400

    did CRUD for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,12 +9,46 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  #making new item
+  def new
+    @item = Item.new
+  end
+
+  #creating it
+  def create
+    @item = Item.create!(item_params)
+    redirect_to items_path
+  end
+
+  #editing an item
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  #updating item
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to items_path(@item)
+  end
+
+  #deleting an item
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
   #adding item to cart
   #WILL ONLY WORK IF USER IS SIGNED IN. ELSE WILL THROW AN ERROR
   def add_to_cart
     @item = Item.find(params[:id])
-    current_user.cart.cart_items.create(item: @item)
-    flash[:notice] = "#{@item.name} was added to the cart"
+    if current_user
+      current_user.cart.cart_items.create(item: @item)
+      flash[:notice] = "#{@item.name} was added to the cart"
+    else
+      flash[:alert] = "You have to be signed in to add items to your cart"
+    end
     redirect_to items_path
   end
 
