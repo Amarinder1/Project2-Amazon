5cf3e98ffcdb67a8b848df02757169aea800750f
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-09-19 14:35:00 -0400

    before sidebar

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,9 @@
 class CartsController < ApplicationController
   def index
-    @cart = current_user.cart
+    if current_user
+      @cart = current_user.cart
+    else
+      flash[:alert] = "You have to be logged in to see the cart"
+    end
   end
 end
